586c0203d37cb39b15557e58f2baace55249354a
Sergey Osiyuk <osiyuksv@mail.ru>
2022-12-20 00:45:59 +0300

    HW2_5num3

---
+++ HW2_5num3/Program.cs
@@ -0,0 +1,16 @@
+﻿// вывести третью цифру рандомного числа
+// в диапазоне от 10 до 100000 или написать, что её нет
+int number = new Random().Next(10, 10000);
+
+Console.WriteLine(number);
+
+if(number > 99)
+{
+ Console.WriteLine("третья цифра " + number % 1000 / 100); 
+}
+else
+{
+Console.WriteLine("третьей цифры нет");
+}
+
+
