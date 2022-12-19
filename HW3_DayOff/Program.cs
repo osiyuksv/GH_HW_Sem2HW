2b12c13ffe11a67f521a7427e32f4a358e88eb2b
Sergey Osiyuk <osiyuksv@mail.ru>
2022-12-20 00:52:11 +0300

    HW3_DayOff

---
+++ HW3_DayOff/Program.cs
@@ -0,0 +1,13 @@
+﻿// вывести вых если от 1 до 7 вых. день нед.
+int number = new Random().Next(1, 8);
+
+Console.WriteLine(number);
+
+if(number < 6)
+{
+ Console.WriteLine("нет"); 
+}
+else
+{
+Console.WriteLine("да");
+}
